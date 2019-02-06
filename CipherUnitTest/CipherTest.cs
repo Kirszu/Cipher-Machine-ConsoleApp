6ff12743dba88fe2ac7a77948b93a353132e1070
Marcin Kirsz <marcinkirsz@wp.pl>
2019-02-06 21:35:37 +0100

    Added Unit Tests for all ciphers

--- CipherUnitTest/CipherTest.cs
+++ CipherUnitTest/CipherTest.cs
@@ -7,28 +7,84 @@ namespace CipherUnitTest
     public class CipherTests
     {
         [TestMethod]
-        public void EncryptValidInput()
+        public void CaesarEncryptValidInput()
         {
             // Arrange
-            CaesarCipher caesar = new CaesarCipher();
+            CaesarCipher cipher = new CaesarCipher();
             string input = "PUSZEK";
             string expected = "UZXEJP";
             // Act
-            string actual = caesar.Encrypt(input);
+            string actual = cipher.Encrypt(input);
 
             // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void DecryptValidInput()
+        public void CaesarDecryptValidInput()
         {
             // Arrange
-            CaesarCipher caesar = new CaesarCipher();
+            CaesarCipher cipher = new CaesarCipher();
             string input = "UZXEJP";
             string expected = "PUSZEK";
             // Act
-            string actual = caesar.Decrypt(input);
+            string actual = cipher.Decrypt(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AffineEncryptValidInput()
+        {
+            // Arrange
+            AffineCipher cipher = new AffineCipher();
+            string input = "PUSZEK";
+            string expected = "IFBCZL";
+            // Act
+            string actual = cipher.Encrypt(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AffineDecryptValidInput()
+        {
+            // Arrange
+            AffineCipher cipher = new AffineCipher();
+            string input = "IFBCZL";
+            string expected = "PUSZEK";
+            // Act
+            string actual = cipher.Decrypt(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VigenereEncryptValidInput()
+        {
+            // Arrange
+            VigenereCipher cipher = new VigenereCipher();
+            string input = "PUSZEK";
+            string expected = "ZILJSD";
+            // Act
+            string actual = cipher.Encrypt(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VigenereDecryptValidInput()
+        {
+            // Arrange
+            VigenereCipher cipher = new VigenereCipher();
+            string input = "ZILJSD";
+            string expected = "PUSZEK";
+            // Act
+            string actual = cipher.Decrypt(input);
 
             // Assert
             Assert.AreEqual(expected, actual);
--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -6,26 +6,6 @@ namespace ConsoleApp15
     {
         static void Main(string[] args)
         {
-            CaesarCipher caesar = new CaesarCipher();
-            string test = "PUSZEK";
-            caesar.Encrypt(test);
-            test = "UZXEJP";
-            caesar.Decrypt(test);
-            caesar.StripAccent(test);
-            AffineCipher affine = new AffineCipher();
-            test = "PUSZEK";
-            affine.Encrypt(test);
-            test = "IFBCZL";
-            affine.Decrypt(test);
-            affine.StripAccent(test);
-            VigenereCipher vigenere = new VigenereCipher();
-            test = "PUSZEK";
-            vigenere.Encrypt(test);
-            test = "ZILJSD";
-            vigenere.Decrypt(test);
-            vigenere.StripAccent(test);
-            Console.ReadLine();
-
 
         }
     }
